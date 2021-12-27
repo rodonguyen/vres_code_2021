89d0c85df9f283eb373b1e5160f02a892f11a852
rodo-github <rodonguyendd@gmail.com>
2021-12-28 00:17:50 +1000

    Add new vPython results, count_stack program Analysis

--- count_stack.py
+++ count_stack.py
@@ -1,31 +1,57 @@
 # Using readlines()
 
-def f(dataset, method, file1):
-    # Compare length and strim
+def count_old_stack(dataset, method, file1):
     nonempty_lines1 = [line.strip("\n") for line in file1 if line != "\n"]
     max_line_1 = len(nonempty_lines1)
     file1.close()
-    # print(nonempty_lines1[0:10])
-
-
+    
     counter1 = {'push': 0, 'pop': 0, 'ext_pop': 0, 'stackadjshrink': 0, 'stackadjgrow': 0}
     start , _ = nonempty_lines1[0].split("->",1)
     for i in range(max_line_1):
-        time , value1 = nonempty_lines1[i].split("->",1)
+        end , value1 = nonempty_lines1[i].split("->",1)
         counter1[value1] += 1
-    exe_time = time - start
+    exe_time = (int(end) - int(start))/1000000
     print(counter1)
-    print('Execution time:', exe_time/1000000)
+    print('Execution time:', exe_time)
 
-    f = open("comparision.txt", "a")
+    f = open("results.txt", "a")
     f.write("Method: " + method + "\n")
     f.write("Dataset: " + str(dataset) + "\n")
-    f.write('Execution time: ' + exe_time + '\n')
+    f.write('Execution time: ' + str(exe_time) + '\n')
     f.write(str(counter1) + '\n\n')
     f.close()
 
-for i in range(9,15+1):
-    dataset = i
-    method = 'pandas'
-    file1 = open("strace_"+ method + "/trace/stack_ds" + str(dataset) + ".txt", "r")
-    f(dataset, method, file1)
+def count_new_stack(dataset, method, file1):
+    nonempty_lines = [line.strip("\n") for line in file1 if line != "\n"]
+    max_line = len(nonempty_lines)
+    file1.close()
+    # print(nonempty_lines1[0:10])
+
+    _, counter = nonempty_lines[-1].split(' ', 1)
+    start , _ = nonempty_lines[0].split("->",1)
+    for i in reversed(range(max_line)):
+        if '->' in nonempty_lines[i]:
+            print(nonempty_lines[i])
+            end, _ = nonempty_lines[i].split("->",1)
+            break
+    exe_time = (int(end) - int(start))/1000000
+    print(counter)
+    print('Execution time:', exe_time)
+
+    f = open("results.txt", "a")
+    f.write("Method: " + method + "\n")
+    f.write("Dataset: " + str(dataset) + "\n")
+    f.write('Execution time: ' + str(exe_time) + '\n')
+    f.write(counter + '\n\n')
+    f.close()
+
+# for i in range(1,15+1):
+#     dataset = i
+#     path = 'strace_pandas/trace/'
+#     file = open(path + 'stack_ds' + str(dataset) + '.txt', "r")
+#     count_old_stack(dataset, path, file)
+
+
+path = 'strace_pandas/'
+file = open(path + 'stack.txt', "r")
+count_old_stack(15, path, file)
--- strace_readline/load_with_readline.py
+++ strace_readline/load_with_readline.py
@@ -1,12 +1,14 @@
 x_values = []
 y_values = []
+
+# [50,100,500,1000,2000,10000,100000]   
 n = 100000
 
 with open('../dataset/dataset_15.csv') as f:
     for i in range(0, n):
-        x, y = f.readline().replace('\n','').split(",")
+        x,y = f.readline().replace('\n','').split(",")
         x_values.append(x)
         y_values.append(y)
 
-print("x values: ", x_values)
-print("y values: ", y_values)
+print(x_values)
+print(y_values)
