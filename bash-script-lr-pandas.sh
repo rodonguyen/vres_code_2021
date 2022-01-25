9693fdfc7423e6473221a35990d0772796735dec
rodo <rodonguyendd@gmail.com>
2022-01-25 22:11:38 +1000

    LR_additional

--- bash-script-lr-pandas.sh
+++ bash-script-lr-pandas.sh
@@ -4,7 +4,7 @@ rm -f vpython.txt
 
 # Start from dataset 1
 count=1
-for i in ./linear_regression_pandas/code/lr_ds*.py
+for i in ./linear_regression_pandas/code/dataset_length_increase/lr_ds_*.py
 do
   # Prompt filename and execute the file using vPython / python3.9
   # Make sure the files are in consecutively-numbered ascending order to have the renaming done correctly. 
@@ -18,7 +18,9 @@ do
 
   # Prompt the new name for easier check
   echo "Renamed to $new"
+  echo "Sleep 2s"
   echo ""
+  sleep 2
 
   # Iterate the 'count' to make new name
   let count=count+1
