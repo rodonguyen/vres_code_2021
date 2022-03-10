4c7d16b0ccdc05fb1846d3aa82ee5e0679f03c61
rodo <rodonguyendd@gmail.com>
2022-03-10 22:45:26 +1000

    Mics

--- utils/single-histogram.py
+++ utils/single-histogram.py
@@ -16,40 +16,29 @@ type = 'pop'
 
 for line in nonempty_lines:
     if any(item in line for item in items):
+        # Get all figures of one item 
         list000 = line.split(',')
-        list000.pop(0)
+        list000.pop(0) # remove the item name
         list000 = sorted(list000)
         # print(str(list000[:10]))
 
+        # Get the number of unique values
         integer_map = map(int, list000)
         integer_list = list(integer_map)
         myset = set(list000)
         sorted_unique_list = sorted(list(set(list000)))
         print('\n' + str(len(myset)) + ' values') #+' => '+str(sorted(myset)))
 
-
-        #####################
-        # confidence intervals
+        # Calculate confidence intervals
         lower =  numpy.percentile(integer_list, 5)
         upper =  numpy.percentile(integer_list, 95)
         
         print(f"{items[count]}:")
         print(f"90% CI:     <{int(lower)},{int(upper)}>")
         print(f"Full range: <{sorted_unique_list[0]},{sorted_unique_list[-1]}>")
-        #####################
 
-        ## OLD VERSION
-        # plt.title('%s dataset: pop count' % (items[count]))
-        # plt.xlabel('value')
-        # plt.ylabel('count')
-
-        # print(plt.hist(list000, alpha=0.8, bins=len(myset)-1))
-        # ymin, ymax = plt.gca().get_ylim() 
-        # print(ymax)
-        # plt.xticks(rotation=60)
-        # plt.show()
-        # plt.close()
-        ##################################
+        # Plot 
+        # Configure chart
         fig, ax = plt.subplots()
         plt.rcParams.update({'font.size': 24})
         plt.title(f"'{Items[count]}' dataset: {type} histogram")
@@ -57,27 +46,27 @@ for line in nonempty_lines:
         plt.xlabel(f"{type} value")
         plt.ylabel('Frequency')
 
-        # Testing: get histogram
+        # Plot the histogram 1st time and 
+        # Save histogram of each item for later use
         histogram, a, b = ax.hist(list000, alpha=0.8, bins=len(myset)-1, color='deepskyblue')
         print(histogram)
-        csv = open('5histogram.csv', 'a')
+        csv = open('count_stuff_results/histogram-push-and-pop.csv', 'a')
         for x in numpy.nditer(histogram):
             csv.write(f"{x},")
         csv.write('\n')
         csv.close()
         # numpy.savetxt('myfile.csv', a, delimiter=',')
 
-
+        # Plot fade Confidence Interval 
         ax.hist(list000, alpha=0.8, bins=len(myset)-1, color='deepskyblue')
         ymin, ymax = ax.get_ylim() 
-        # print('ymax:', ymax)
-        
         # CI zone
         str_lower, str_upper = str(int(lower)), str(int(upper))
         ax.fill_betweenx([0,ymax],[str_lower, str_lower], [str_upper, str_upper] ,
                         facecolor ='orange', alpha = 0.2)
-        
 
+        
+        # Plot the histogram 2nd time to overlay the CI layer 
         ax.hist(list000, alpha=0.8, bins=len(myset)-1, color='deepskyblue') # Repeat 'hist' to put important columns front
         plt.xticks(rotation=60)
         plt.show()
