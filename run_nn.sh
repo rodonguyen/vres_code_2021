84205edde61b1181fc04ff0d2bed9ee6a7622116
Rodo <rodonguyendd@gmail.com>
2022-07-13 09:37:24 +1000

    Collect nn traces from all datasets (with and without RandomSeed)

--- run_nn.sh
+++ run_nn.sh
@@ -5,11 +5,11 @@ rm -f vpython*.txt
 
 # Create directories to store traces
 
-dir001="trace_car_withRandom"
-dir002="trace_diabetes_withRandom"
+# dir001="trace_car_withRandom"
+# dir002="trace_diabetes_withRandom"
 # dir003="trace_energy_withRandom"
 # dir004="trace_house_withRandom"
-# dir005="trace_medical_withRandom"
+dir005="trace_medical_withRandom"
 
 # dir001="trace_car_noRandom"
 # dir002="trace_diabetes_noRandom"
@@ -17,11 +17,11 @@ dir002="trace_diabetes_withRandom"
 # dir004="trace_house_noRandom"
 # dir005="trace_medical_noRandom"
 
-mkdir neural_network/$dir001
-mkdir neural_network/$dir002
+# mkdir neural_network/$dir001
+# mkdir neural_network/$dir002
 # mkdir neural_network/$dir003
 # mkdir neural_network/$dir004
-# mkdir neural_network/$dir005
+mkdir neural_network/$dir005
 
 run_num=110
 
@@ -29,42 +29,42 @@ run_num=110
 # Make sure the files are in consecutively-numbered ascending order to have the renaming done correctly. 
 # Otherwise, the renaming will be a mess.
 
-# ################################ CAR #################################
-for (( i=1; i<=$run_num; i++ ))
-do
-    begin=$(printf "Executing car.py (%03d)" "$i")
-    echo "_____________________"
-    echo "$begin"
-    python3.9 ./neural_network/car.py
-    killall -9 python3.9
+# # ################################ CAR #################################
+# for (( i=1; i<=$run_num; i++ ))
+# do
+#     begin=$(printf "Executing car.py (%03d)" "$i")
+#     echo "_____________________"
+#     echo "$begin"
+#     python3.9 ./neural_network/car.py
+#     killall -9 python3.9
 
-    # Renaming
-    new=$(printf "vpython_car_%03d.txt" "$i")
-    mv vpython.txt "$new"
-    echo "Renamed to $new"
-    echo 
-done
-# Move all trace files to a folder
-mv vpython*.txt neural_network/$dir001
+#     # Renaming
+#     new=$(printf "vpython_car_%03d.txt" "$i")
+#     mv vpython.txt "$new"
+#     echo "Renamed to $new"
+#     echo 
+# done
+# # Move all trace files to a folder
+# mv vpython*.txt neural_network/$dir001
 
 
-############################### DIABETES #################################
-for (( i=1; i<=$run_num; i++ ))
-do
-    begin=$(printf "Executing diabetes.py (%03d)" "$i")
-    echo "_____________________"
-    echo "$begin"
-    python3.9 ./neural_network/diabetes.py
-    killall -9 python3.9
+# ############################### DIABETES #################################
+# for (( i=1; i<=$run_num; i++ ))
+# do
+#     begin=$(printf "Executing diabetes.py (%03d)" "$i")
+#     echo "_____________________"
+#     echo "$begin"
+#     python3.9 ./neural_network/diabetes.py
+#     killall -9 python3.9
 
-    # Renaming
-    new=$(printf "vpython_diabetes_%03d.txt" "$i")
-    mv vpython.txt "$new"
-    echo "Renamed to $new"
-    echo 
-done
-# Move all trace files to a folder
-mv vpython*.txt neural_network/$dir002
+#     # Renaming
+#     new=$(printf "vpython_diabetes_%03d.txt" "$i")
+#     mv vpython.txt "$new"
+#     echo "Renamed to $new"
+#     echo 
+# done
+# # Move all trace files to a folder
+# mv vpython*.txt neural_network/$dir002
 
 
 ################################ ENERGY #################################
@@ -107,22 +107,24 @@ mv vpython*.txt neural_network/$dir002
 
 
 # ############################# MEDICAL ####################################
-# for (( i=1; i<=$run_num; i++ ))
-# do
-#     begin=$(printf "Executing medical.py (%03d)" "$i")
-#     echo "_____________________"
-#     echo "$begin"
-#     python3.9 ./neural_network/medical.py
-#     killall -9 python3.9
+for (( i=1; i<=$run_num; i++ ))
+do
+    begin=$(printf "Executing medical.py (%03d)" "$i")
+    echo "_____________________"
+    echo "$begin"
+    python3.9 ./neural_network/medical.py
+    killall -9 python3.9
 
-#     # Renaming
-#     new=$(printf "vpython_medical_%03d.txt" "$i")
-#     mv vpython.txt "$new"
-#     echo "Renamed to $new"
-#     echo 
-# done
-# # Move all trace files to a folder
-# mv vpython*.txt neural_network/$dir005
+    # Renaming
+    new=$(printf "vpython_medical_%03d.txt" "$i")
+    mv vpython.txt "$new"
+    echo "Renamed to $new"
+    echo 
+done
+# Move all trace files to a folder
+mv vpython*.txt neural_network/$dir005
+
+
 
 # python3.8 neural_network/run_analysis_script.py
 spd-say done
