acf97923a80f941de6d38ba98d435d106d7ab406
rodonguyen <rodonguyendd@gmail.com>
2022-09-09 00:46:58 +1000

    Done MNIST report

--- programs/neural_network/mnist/code_test/mnist_26.py
+++ programs/neural_network/mnist/code_test/mnist_26.py
@@ -66,10 +66,11 @@ transform=transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.1307,), (0.3081,))
     ])
-dataset1 = datasets.MNIST('data', train=True, # download=True,
+dataset1 = datasets.MNIST('data', train=True, #download=True,
                 transform=transform)
 dataset2 = datasets.MNIST('data', train=False,
                 transform=transform)
+
 print('Train on', int(26), 'images.')
 train_loader = list(torch.utils.data.DataLoader(dataset1))[:26]
 test_loader = list(torch.utils.data.DataLoader(dataset2))[:10]
